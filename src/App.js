225e99c150b4bddd7a1323ff4d429cdbe50eefb7
shahzaibansari123 <ansarishahzaib567@gmail.com>
2023-01-07 04:09:36 +0500

    working on it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
+import './App.css'
 
+
+
+import Sidebar from './Components/Sidebar/Sidebar';
 function App() {
-  return <div>
-    
-  </div>;
+  return <div >
+
+<Sidebar />
+  </div>
 }
 
 export default App;
